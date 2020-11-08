5c53cbcbba38f1e0daf6cb78e6bb456ce89ff722
Sirine <sirine.khelifi1@outlook.com>
2020-11-08 02:49:31 -0500

    finished queries

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -4,7 +4,7 @@ FROM employees
 INNER JOIN salaries 
 ON (employees.emp_no=salaries.emp_no);
 
--- List first name, last name, and hire date for employees who were hired in 1986.
+--List first name, last name, and hire date for employees who were hired in 1986.
 SELECT first_name, last_name, hire_date FROM employees WHERE hire_date Between '1986/01/01' AND '1986/12/31';
 
 --List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
@@ -22,4 +22,26 @@ ON (employees.emp_no=dept_emp.emp_no)
 INNER JOIN departments
 ON (departments.dept_no=dept_emp.dept_no);
 
--- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex FROM employees WHERE first_name= 'Hercules' and last_name LIKE 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, last_name, first_name, dept_name 
+FROM employees INNER JOIN dept_emp
+ON (employees.emp_no=dept_emp.emp_no)
+INNER JOIN departments
+ON (departments.dept_no=dept_emp.dept_no)
+WHERE dept_name= 'Sales';
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, last_name, first_name, dept_name 
+FROM employees INNER JOIN dept_emp
+ON (employees.emp_no=dept_emp.emp_no)
+INNER JOIN departments
+ON (departments.dept_no=dept_emp.dept_no)
+WHERE dept_name= 'Sales' OR dept_name= 'Development';
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT COUNT (last_name) FROM employees
+GROUP BY employees.last_name
+ORDER BY employees.last_name DESC;
